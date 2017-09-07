e6eccb586a7c8b16377960f7fcb6a8ffbe3fd4e8
Ben Chachaj <benedict.chachaj@student.fairfield.edu>
2017-09-07 16:12:48 -0400

    3d objects responding correctly, remove unused code and start set up variations

--- app/js/index.js
+++ app/js/index.js
@@ -9,19 +9,15 @@ window.onload = () => {
   let viz  =  new Visualizer();
   viz.initialize();
   viz.createObjects();
+  viz.processAudio();
 };
-
-
-
-//document on load is going to generate everything needed
-
 
 
 function Visualizer() {
   //set some constants
 
   //(at this time) arbitrary number of bars
-  this.objNum = 60;
+  this.objNum = 80;
 
   // //store 3d objects to be manipulated 
   this.objArray = new Array();
@@ -42,26 +38,25 @@ Visualizer.prototype.initialize = function(){
   let HEIGHT = window.innerHeight;
   let WIDTH = window.innerWidth;
 
-  renderer = new THREE.WebGLRenderer({canvas: document.getElementById('canvas')});
+  this.renderer = new THREE.WebGLRenderer({canvas: document.getElementById('canvas')});
 
-  renderer.setSize( WIDTH, HEIGHT );
+  this.renderer.setSize( WIDTH, HEIGHT );
 
-  renderer.setClearColor(0xffffff);
+  this.renderer.setClearColor(0x313a49);
 
-  renderer.setPixelRatio(window.devicePixelRatio);
+  this.renderer.setPixelRatio(window.devicePixelRatio);
 
   this.scene = new THREE.Scene();
 
 
-  camera = new THREE.PerspectiveCamera( 75, WIDTH / HEIGHT, 1, 8000);
-  camera.position.z = 50;
+  this.camera = new THREE.PerspectiveCamera( 75, WIDTH / HEIGHT, 1, 8000);
+  this.camera.position.z = 50;
 
 
   //updating the camera
 
   geometry = new THREE.BoxGeometry( 100, 100, 100, 100 );
 
-  // geometry = new THREE.BufferGeometry();
 
   material = new THREE.MeshLambertMaterial( { color: 0xBADA55 } );
 
@@ -78,8 +73,6 @@ Visualizer.prototype.initialize = function(){
   var directLight = new THREE.DirectionalLight(0xffffff, 0.5);
   this.scene.add(directLight)
 
-  // let audio = fileUpload();
-  // console.log(audio);
   let that = this;
 
   function animate() {
@@ -88,7 +81,7 @@ Visualizer.prototype.initialize = function(){
       mesh.rotation.x += 0.01;
       mesh.rotation.y += 0.02;
 
-      renderer.render(that.scene, camera );
+      that.renderer.render(that.scene, that.camera );
   }
   animate();
 
@@ -98,7 +91,6 @@ Visualizer.prototype.initialize = function(){
 Visualizer.prototype.createObjects = function() {
 
   for (let i = 0; i < this.objNum; i++) {
-    //one geometry plz 
 
     //precursors for 3d object
     let geometry = new THREE.BoxGeometry( 0.7, 0.7, 0.7);
@@ -106,7 +98,7 @@ Visualizer.prototype.createObjects = function() {
 
     //assign array position to new 3d objects
     this.objArray[i] = new THREE.Mesh( geometry, material );
-    this.objArray[i].position.set(i - this.objNum, 5, 10);
+    this.objArray[i].position.set(((i - this.objNum) * 1.3), 5, 10);
     
 
     this.scene.add(this.objArray[i]);
@@ -115,174 +107,67 @@ Visualizer.prototype.createObjects = function() {
 };
 
 
-Visualizer.prototype.updateObjects = function() {
+Visualizer.prototype.processAudio = function() {
 
-  //seperate getting obj?
-  //get data, then affect the objArray. Scale on z - axis?
-}
-
-
-//Getting audio fall under the visualizer class? how else to access it?
-
-
-Visualizer.prototype.processAudo = function() {
   let analyser;
   const context = new AudioContext();
   const audio = document.getElementById('audio-player');
 
   const audioSource = context.createMediaElementSource(audio);
+  audioSource.crossOrigin = "anonymous";
+
 
   this.analyser = context.createAnalyser();
 
-  audioSource.connect(analyser);
+  this.analyser.fftSize = 8192;
+  this.analyser.smoothingTimeConstant = 0.8;
+
+  audioSource.connect(this.analyser);
   //connect to actually hear sound when played
   audioSource.connect(context.destination); 
 
-  var frequencyData = new Uint8Array(analyser.frequencyBinCount);
+  var frequencyData = new Uint8Array(this.analyser.frequencyBinCount);
 
-  //function renderframe == gets byte frequency, request animation frame 
-  this.renderframe(frequencyData);
+
+  let that = this;
 
   function renderFrame() {
     requestAnimationFrame( renderFrame );
 
-    this.analyser.getByteFrequencyData(frequencyData);
+    that.analyser.getByteFrequencyData(frequencyData);
+
+    const offset = Math.round(frequencyData.length / that.objNum);
+
+    that.renderer.render(that.scene, that.camera);
+
+    for (var i = 0; i < that.objNum; i++) {
+
+      debugger;
+      let value = frequencyData[i * offset] / 5;
+      value = value < 1 ? 1 : value;
+      that.objArray[i].scale.y = value;
+      that.objArray[i].scale.z = value * 0.2;
+
+      if( value > 37) {
+        that.objArray[i].material.color.setHex( 0xd6399a);
+      }
+      else if (value > 30 && value <= 37) {
+        that.objArray[i].material.color.setHex( 0xdd1713);
+      } else if (value < 30 && value > 20 ){
+        that.objArray[i].material.color.setHex( 0xff960c);
+      } else if (value < 20 && value > 10) {
+         that.objArray[i].material.color.setHex( 0xffdc44);
+      } else {
+          that.objArray[i].material.color.setHex( 0xBADA55);
+      }
+    }
   } 
+
 
   renderFrame();
 }
 
 
-
-// Visualizer.prototype.renderFrame = function(frequencyData) {
-//  requestAnimationFrame( renderFrame );
-//  this.analyser.getByteFrequencyData(frequencyData);
-
-//   const offset = frequencyData / this.numObj;
-
-//  for (var i = 0; i < this.numObj; i++) {
-//     let j = 
-//  }
-// }
-
-
-
-
-
-
-
-
- function renderFrame() {
-  //        requestAnimationFrame(renderFrame);
-   //       analyser.getByteFrequencyData(frequencyData);
-
-        //STIL
-  //        var width = Math.floor(1/frequencyData, 10);
-
-
-      //ITERATE THROUGH AND SCALE THE Z AXIS 
-
-      // how much scale: could be lengh of freq / numObj?
-
-      const offset = frequencyData / this.numObj;
-
-   //     for (let i = 0; i < this.numObj; i++) {
-   //       let value = frequencyData[i];
-   //       var percent = value / 256;
-   //       height = HEIGHT * percent;
-
-            //***dont need positioning offset for THREE 
-   //       offset = HEIGHT - height - 1;
-
-        //*dont need width either
-   //       var bar_width = (WIDTH/(analyser.frequencyBinCount / 2));
-
-
-   //       ctx.fillRect(i * bar_width, offset, bar_width, height); 
-
-   //     }
-  //       }
-  //       // audio.play();
-  //       renderFrame();
-  //   };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// window.onload = getAudio();
-
-  //   function getAudio() {
-  //       var context = new AudioContext();
-  //       var audio = document.getElementById('audio-player');
-  //       var audioSource = context.createMediaElementSource(audio);
-  //    // audioSource.crossOrigin = "anonymous";
-  //       var analyser = context.createAnalyser();
-  //       analyser.fftSize = 8192;
-  //       analyser.smoothingTimeConstant = 0.8;
-  //       audioSource.connect(analyser);
-  //       audioSource.connect(context.destination);
-
-  //       var frequencyData = new Uint8Array(analyser.frequencyBinCount);
-  //           console.log(analyser.frequencyBinCount);
-
-  //        ctx.clearRect(0,0,canvas.width, canvas.height);
-
-  //        var WIDTH = canvas.height;
-    // var HEIGHT = canvas.width;
-
-
-  //   function renderFrame() {
-  //        requestAnimationFrame(renderFrame);
-   //       analyser.getByteFrequencyData(frequencyData);
-
-        //STIL
-  //        var width = Math.floor(1/frequencyData, 10);
-
-
-      //ITERATE THROUGH AND SCALE THE Z AXIS 
-
-      // how much scale: could be lengh of freq / numObj?
-
-      const offset = frequencyData / this.numObj;
-
-   //     for (let i = 0; i < this.numObj; i++) {
-   //       let value = frequencyData[i];
-   //       var percent = value / 256;
-   //       height = HEIGHT * percent;
-
-            //***dont need positioning offset for THREE 
-   //       offset = HEIGHT - height - 1;
-
-        //*dont need width either
-   //       var bar_width = (WIDTH/(analyser.frequencyBinCount / 2));
-
-
-   //       ctx.fillRect(i * bar_width, offset, bar_width, height); 
-
-   //     }
-  //       }
-  //       // audio.play();
-  //       renderFrame();
-  //   };
-
-    // console.log(global);
-
-    // for(let j = 0; j < global.length; j++) {
-    //  console.log(global[j]);
-    // }
-    // console.log(renderer);
 
 
 
