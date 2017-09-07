f6a3ac72cc8c9f564548395e8c28c207855b1c56
Ben Chachaj <benedict.chachaj@student.fairfield.edu>
2017-09-07 15:02:55 -0400

    refactoring testing script into an OOD

--- app/js/index.js
+++ app/js/index.js
@@ -31,6 +31,7 @@ function Visualizer() {
   this.camera; 
   this.renderer;
   this.controls;
+  this.analyser;
 
 }
 
@@ -61,8 +62,6 @@ Visualizer.prototype.initialize = function(){
   geometry = new THREE.BoxGeometry( 100, 100, 100, 100 );
 
   // geometry = new THREE.BufferGeometry();
-
-
 
   material = new THREE.MeshLambertMaterial( { color: 0xBADA55 } );
 
@@ -99,31 +98,113 @@ Visualizer.prototype.initialize = function(){
 Visualizer.prototype.createObjects = function() {
 
   for (let i = 0; i < this.objNum; i++) {
-    // console.log(i);
-
     //one geometry plz 
 
-
+    //precursors for 3d object
     let geometry = new THREE.BoxGeometry( 0.7, 0.7, 0.7);
-
-  // geometry = new THREE.BufferGeometry();
-
-
-
     let material = new THREE.MeshLambertMaterial( { color: 0xBADA55 } );
 
+    //assign array position to new 3d objects
     this.objArray[i] = new THREE.Mesh( geometry, material );
     this.objArray[i].position.set(i - this.objNum, 5, 10);
     
 
     this.scene.add(this.objArray[i]);
-    //export
-    // this.objArray.push(mesh);
   }
 
 };
 
 
+Visualizer.prototype.updateObjects = function() {
+
+  //seperate getting obj?
+  //get data, then affect the objArray. Scale on z - axis?
+}
+
+
+//Getting audio fall under the visualizer class? how else to access it?
+
+
+Visualizer.prototype.processAudo = function() {
+  let analyser;
+  const context = new AudioContext();
+  const audio = document.getElementById('audio-player');
+
+  const audioSource = context.createMediaElementSource(audio);
+
+  this.analyser = context.createAnalyser();
+
+  audioSource.connect(analyser);
+  //connect to actually hear sound when played
+  audioSource.connect(context.destination); 
+
+  var frequencyData = new Uint8Array(analyser.frequencyBinCount);
+
+  //function renderframe == gets byte frequency, request animation frame 
+  this.renderframe(frequencyData);
+
+  function renderFrame() {
+    requestAnimationFrame( renderFrame );
+
+    this.analyser.getByteFrequencyData(frequencyData);
+  } 
+
+  renderFrame();
+}
+
+
+
+// Visualizer.prototype.renderFrame = function(frequencyData) {
+//  requestAnimationFrame( renderFrame );
+//  this.analyser.getByteFrequencyData(frequencyData);
+
+//   const offset = frequencyData / this.numObj;
+
+//  for (var i = 0; i < this.numObj; i++) {
+//     let j = 
+//  }
+// }
+
+
+
+
+
+
+
+
+ function renderFrame() {
+  //        requestAnimationFrame(renderFrame);
+   //       analyser.getByteFrequencyData(frequencyData);
+
+        //STIL
+  //        var width = Math.floor(1/frequencyData, 10);
+
+
+      //ITERATE THROUGH AND SCALE THE Z AXIS 
+
+      // how much scale: could be lengh of freq / numObj?
+
+      const offset = frequencyData / this.numObj;
+
+   //     for (let i = 0; i < this.numObj; i++) {
+   //       let value = frequencyData[i];
+   //       var percent = value / 256;
+   //       height = HEIGHT * percent;
+
+            //***dont need positioning offset for THREE 
+   //       offset = HEIGHT - height - 1;
+
+        //*dont need width either
+   //       var bar_width = (WIDTH/(analyser.frequencyBinCount / 2));
+
+
+   //       ctx.fillRect(i * bar_width, offset, bar_width, height); 
+
+   //     }
+  //       }
+  //       // audio.play();
+  //       renderFrame();
+  //   };
 
 
 
@@ -140,6 +221,75 @@ Visualizer.prototype.createObjects = function() {
 
 
 
+// window.onload = getAudio();
+
+  //   function getAudio() {
+  //       var context = new AudioContext();
+  //       var audio = document.getElementById('audio-player');
+  //       var audioSource = context.createMediaElementSource(audio);
+  //    // audioSource.crossOrigin = "anonymous";
+  //       var analyser = context.createAnalyser();
+  //       analyser.fftSize = 8192;
+  //       analyser.smoothingTimeConstant = 0.8;
+  //       audioSource.connect(analyser);
+  //       audioSource.connect(context.destination);
+
+  //       var frequencyData = new Uint8Array(analyser.frequencyBinCount);
+  //           console.log(analyser.frequencyBinCount);
+
+  //        ctx.clearRect(0,0,canvas.width, canvas.height);
+
+  //        var WIDTH = canvas.height;
+    // var HEIGHT = canvas.width;
+
+
+  //   function renderFrame() {
+  //        requestAnimationFrame(renderFrame);
+   //       analyser.getByteFrequencyData(frequencyData);
+
+        //STIL
+  //        var width = Math.floor(1/frequencyData, 10);
+
+
+      //ITERATE THROUGH AND SCALE THE Z AXIS 
+
+      // how much scale: could be lengh of freq / numObj?
+
+      const offset = frequencyData / this.numObj;
+
+   //     for (let i = 0; i < this.numObj; i++) {
+   //       let value = frequencyData[i];
+   //       var percent = value / 256;
+   //       height = HEIGHT * percent;
+
+            //***dont need positioning offset for THREE 
+   //       offset = HEIGHT - height - 1;
+
+        //*dont need width either
+   //       var bar_width = (WIDTH/(analyser.frequencyBinCount / 2));
+
+
+   //       ctx.fillRect(i * bar_width, offset, bar_width, height); 
+
+   //     }
+  //       }
+  //       // audio.play();
+  //       renderFrame();
+  //   };
+
+    // console.log(global);
+
+    // for(let j = 0; j < global.length; j++) {
+    //  console.log(global[j]);
+    // }
+    // console.log(renderer);
+
+
+
+
+
+
+//bottom of file
 
 
 
